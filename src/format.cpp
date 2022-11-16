634fa924075631f0b79758e62a59f5ccbc659183
Nikola Milosevic <nikolamilosevic441@gmail.com>
2022-11-16 13:59:48 -0500

    update string_printf
    We don't need to allocate extra room because since C++11 the str.data()
    array includes the null terminator.

--- src/format.cpp
+++ src/format.cpp
@@ -8,18 +8,15 @@ std::string string_printf(const char *fmt, ...) {
     va_start(ap1, fmt);
     int n = vsnprintf(nullptr, 0, fmt, ap1);
 
-                                          // because we can't stop sprintf from writing nul-terminators, we have to:
-    std::string str(n+1, '\0');           // allocate a string with n+1 length,
+    std::string str(n, '\0');
     va_copy(ap2, ap1);
-    vsnprintf(str.data(), n+1, fmt, ap2); // then vsnprintf will overwrite that n+1'th character with '\0',
+    vsnprintf(str.data(), n+1, fmt, ap2); // vsnprintf will write the null terminator into str[n]
     va_end(ap2);
     va_end(ap1);
-    str.erase(str.end()-1);               // finally, erase the last character because we don't want any extra NULs
-                                          // (std::string already gives us one at str.end())
 
+    ASSERT(str[str.length()] == 0, "string should be null terminated");
     ASSERT(std::all_of(str.begin(), str.end(), [](auto c) { return c != '\0'; }),
-           "vsprintf should have overwritten all the NUL characters");
-    ASSERT(str.length() == n, "string length should be n");
+           "vsnprintf should have overwritten all the NUL characters");
 
     return str;
 }
