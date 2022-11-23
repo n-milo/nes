387e54bebdd7c125d9fc7d9d72ef4c497f15d2d7
Nikola Milosevic <nikolamilosevic441@gmail.com>
2022-11-22 19:36:09 -0500

    change modulo to bitwise and

--- src/bus.cpp
+++ src/bus.cpp
@@ -4,7 +4,7 @@ void Bus::write(uint16 addr, uint8 data) {
     if (cartridge.cpu_write(addr, data)) {
         // TODO
     } else if (addr >= RAM_START && addr <= RAM_END) {
-        ram[addr % 0x7ff] = data;
+        ram[addr & 0x7ff] = data;
     } else if (addr >= PPU_START && addr <= PPU_END) {
         ppu.cpu_write(addr & 0x7, data);
     } else {
@@ -16,7 +16,7 @@ uint8 Bus::read(uint16 addr) {
     if (auto data = cartridge.cpu_read(addr); data.has_value()) {
         return *data;
     } else if (addr >= RAM_START && addr <= RAM_END) {
-        return ram[addr % 0x7ff];
+        return ram[addr & 0x7ff];
     } else if (addr >= PPU_START && addr <= PPU_END) {
         return ppu.cpu_read(addr & 0x7);
     } else {
