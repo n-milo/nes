69962e1a7cb74c2c23821cd3bf6cc24b65331a4c
Nikola Milosevic <nikolamilosevic441@gmail.com>
2022-11-24 15:49:42 -0500

    fix emscripten build
    The text is red on desktop but blue on emscripten? What??

--- build_emscripten.sh
+++ build_emscripten.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # my emscripten "build system"
-emcc src/r6502.cpp src/bus.cpp src/cartridge.cpp src/format.cpp src/main.cpp src/font.cpp \
+emcc src/r6502.cpp src/bus.cpp src/cartridge.cpp src/format.cpp src/main.cpp src/font.cpp src/ppu.cpp src/gfx.cpp \
   -std=c++17 \
   -Iinclude/ -I/opt/homebrew/include/ -include common.h \
   --preload-file monogram-bitmap.json \
+  --preload-file roms/ \
   -g -O2 \
   -s USE_SDL=2 \
   -sALLOW_MEMORY_GROWTH \
